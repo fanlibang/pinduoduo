5fe9dd510593c507bb28ab32aacc4ff043724c57
zhang.jiang <zhang.jiang@qianka.com>
2019-04-29 10:29:52 +0800

    fix

--- src/Request/DdkGoodsDetailRequest.php
+++ src/Request/DdkGoodsDetailRequest.php
@@ -17,20 +17,32 @@ class DdkGoodsDetailRequest extends Request
      */
     private $goodsIdList;
 
-    public function setType($type)
-    {
-        $this->type = $type;
-    }
+    /**
+     * 自定义参数，为链接打上自定义标签。自定义参数最长限制64个字节。
+     * @var
+     */
+    private $customParameters;
 
-    public function setGoodsIdList($goodsIdList)
-    {
-        $this->goodsIdList = $goodsIdList;
-    }
+    /**
+     * 招商多多客ID
+     * @var
+     */
+    private $zsduoId;
 
+    /**
+     * 佣金优惠券对应推广类型，3：专属 4：招商
+     * @var
+     */
+    private $planType;
 
     public function getType()
     {
         return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
     }
 
     public function getGoodsIdList()
@@ -38,11 +50,49 @@ class DdkGoodsDetailRequest extends Request
         return $this->goodsIdList;
     }
 
+    public function setGoodsIdList($goodsIdList)
+    {
+        $this->goodsIdList = $goodsIdList;
+    }
+
+    public function setZsduoId($zsduoId)
+    {
+        $this->zsduoId = $zsduoId;
+    }
+
+    public function getZsduoId()
+    {
+        return $this->zsduoId;
+    }
+
+    public function setCustomParameters($customParameters)
+    {
+        $this->customParameters = $customParameters;
+    }
+
+    public function getCustomParameters()
+    {
+        return $this->customParameters;
+    }
+
+    public function setPlanType($planType)
+    {
+        $this->planType = $planType;
+    }
+
+    public function getPlanType()
+    {
+        return $this->planType;
+    }
+
     public function getParams()
     {
         return [
-            'type'          => $this->type,
-            'goods_id_list' => $this->goodsIdList
+            'type'              => $this->type,
+            'goods_id_list'     => $this->goodsIdList,
+            'custom_parameters' => $this->customParameters,
+            'zs_duo_id'         => $this->zsduoId,
+            'plan_type'         => $this->planType,
         ];
     }
 }
